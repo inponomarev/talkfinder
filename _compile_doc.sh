504b9b0b92b347fee4a7285bfe4f1b1ecd643d68
iponomarev <iponomarev@mail.ru>
2020-05-31 21:21:36 +0300

    change mv to cp because of permission limitation

--- _compile_doc.sh
+++ _compile_doc.sh
@@ -20,19 +20,19 @@ cd ..
 # Подготовка json-файлов с нужной иерархией
 docker run --rm -v $(pwd):/documents/ lihame/course-doc \
   nunjucks src/njk/talk2id.njk target/talks.json -p . -e json -o target
-mv target/src/njk/talk2id.json ./target/talks-by-id.json;
+cp target/src/njk/talk2id.json ./target/talks-by-id.json;
 
 docker run --rm -v $(pwd):/documents/ lihame/course-doc \
   nunjucks src/njk/author2id.njk target/speakers.json -p . -e json -o target
-mv target/src/njk/author2id.json ./target/speakers-by-id.json;
+cp target/src/njk/author2id.json ./target/speakers-by-id.json;
 
 docker run --rm -v $(pwd):/documents/ lihame/course-doc \
   nunjucks src/njk/ev_type2id.njk target/event-types.json -p . -e json -o target
-mv target/src/njk/ev_type2id.json ./target/ev_types-by-id.json;
+cp target/src/njk/ev_type2id.json ./target/ev_types-by-id.json;
 
 docker run --rm -v $(pwd):/documents/ lihame/course-doc \
   nunjucks src/njk/ev_type2ev.njk ./target/events.json -p . -e json -o target
-mv target/src/njk/ev_type2ev.json ./target/ev_type2ev.json;
+cp target/src/njk/ev_type2ev.json ./target/ev_type2ev.json;
 
 docker run --rm -v $(pwd):/documents/ lihame/course-doc \
   jq -s '.[0] * .[1] * .[2] * .[3]' target/ev_types-by-id.json target/speakers-by-id.json \
@@ -41,7 +41,7 @@ docker run --rm -v $(pwd):/documents/ lihame/course-doc \
 # Сборка
 docker run --rm -v $(pwd):/documents/ lihame/course-doc \
   nunjucks src/njk/talks.njk ./target/combined.json -p . -e adoc -o target
-mv target/src/njk/talks.adoc ./target/talks_pre.adoc;
+cp target/src/njk/talks.adoc ./target/talks_pre.adoc;
 
 docker run --rm -v $(pwd):/documents/ lihame/course-doc \
   asciidoctor -r ./src/extensions/feat-1338.rb \
