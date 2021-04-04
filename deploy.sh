d6ab135f04ba11be3b8d7164b4873023a7baa8cf
Ivan Ponomarev <iponomarev@mail.ru>
2021-04-04 17:11:11 +0300

    add deploy to server

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ set -x
 openssl enc -a -d -aes-256-cbc -in deploy_rsa.enc -out deploy_rsa -pass env:DECYPHER_KEY
 chmod 400 deploy_rsa
 
-ssh -i deploy_rsa -o "StrictHostKeyChecking no" root@jugspeakers.online "mkdir -p /opt/talkfinder/talkfinder-${TRAVIS_BUILD_NUMBER}"
+ssh -i deploy_rsa -o "StrictHostKeyChecking no" root@jugspeakers.online "mkdir -p /opt/talkfinder/talkfinder-${GH_BUILD_NUMBER}"
 
-scp -i deploy_rsa -o "StrictHostKeyChecking no" talkfinder.tgz root@jugspeakers.online:/opt/talkfinder/talkfinder-${TRAVIS_BUILD_NUMBER}/talkfinder.tgz
+scp -i deploy_rsa -o "StrictHostKeyChecking no" talkfinder.tgz root@jugspeakers.online:/opt/talkfinder/talkfinder-${GH_BUILD_NUMBER}/talkfinder.tgz
 
-ssh -i deploy_rsa root@jugspeakers.online "cd /opt/talkfinder/talkfinder-${TRAVIS_BUILD_NUMBER}; tar xzf talkfinder.tgz --strip-components=2; rm talkfinder.tgz; cd ..; ln -sfn talkfinder-${TRAVIS_BUILD_NUMBER} talkfinder"
+ssh -i deploy_rsa root@jugspeakers.online "cd /opt/talkfinder/talkfinder-${GH_BUILD_NUMBER}; tar xzf talkfinder.tgz --strip-components=2; rm talkfinder.tgz; cd ..; ln -sfn talkfinder-${GH_BUILD_NUMBER} talkfinder"
 
 rm -f deploy_rsa
